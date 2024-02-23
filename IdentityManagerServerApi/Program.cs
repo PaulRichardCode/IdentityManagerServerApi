cb04ff26133ad91cd63e48c0ddd33c6b17db49d8
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-02-23 12:27:27 +0100

    This is going well

--- IdentityManagerServerApi/Program.cs
+++ IdentityManagerServerApi/Program.cs
@@ -1,3 +1,6 @@
+using IdentityManagerServerApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string is not Found"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
