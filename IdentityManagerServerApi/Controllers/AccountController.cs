bbfed9e8889416b22bdb3db9729e8cf6dfeca944
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-02-28 13:57:45 +0100

    adding controller

---
+++ IdentityManagerServerApi/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1.Contracts;
+using ClassLibrary1.DTOs; 
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagerServerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(IUserAccount userAccount) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserDTO userDTO)
+        {
+            var response = await userAccount.CreateAccount(userDTO);
+            return Ok(response);
+        }
+    }
+}
